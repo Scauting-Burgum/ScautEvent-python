5b85a1bbada0768c43807ca6149171875c1e1d3b
Thomas Egbert Duursma <thomasduursma@outlook.com>
2018-06-14 15:26:08 +0200

    Update common.py
    * Changed the name of `Event.value` to `Event.data`.
    * Added the method; `get_pipeline`.

--- common.py
+++ common.py
@@ -1,17 +1,23 @@
-from .ScautNet import ConversionFilter
+from .ScautNet import ConversionFilter, Connection, TextFilter
 import json
 
 class Event:
-    def __init__(self, name, value):
+    def __init__(self, name, data):
         self.name = name
-        self.value = value
+        self.data = data
 
 def event_from_json(_json):
     return Event(**json.loads(_json))
 
 def event_to_json(event):
-    return json.dumps({"name":event.name, "value":event.value})
+    return json.dumps({"name":event.name, "data":event.data})
 
 class EventFilter(ConversionFilter):
     def __init__(self):
         super().__init__(event_from_json, event_to_json)
+
+def get_pipeline(socket):
+    connection = Connection(socket)
+    text_filter = TextFilter()
+    event_filter = EventFilter()
+    return PipeLine(connection, text_filter, event_filter)
